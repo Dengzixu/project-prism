18b4a9ef11aa5bcd824725f539a0ccb0f51e991e
Deng.Zixu <admin@dengzixu.com>
2021-11-28 20:55:38 +0800

    初始化项目结构

---
+++ src/main/java/net/dengzixu/java/project_prism/ProjectPrismApplication.java
@@ -0,0 +1,14 @@
+package net.dengzixu.java.project_prism;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPrismApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectPrismApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/dengzixu/java/project_prism/ProjectPrismApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dengzixu.java.project_prism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPrismApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
